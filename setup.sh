49eb4dfdfda1bd9b06d07144f87bc6dcc9fe842b
tester <tester@updoc.lan>
2018-10-02 12:17:06 -0600

    add bash to setup

--- setup.sh
+++ setup.sh
@@ -1,15 +1,19 @@
 #!/bin/bash -ex
 
+echo "update clouldformat to s3"
+aws s3 cp aws-swarm.json s3://updoc-iac/aws-swarm.json --acl public-read
+
 echo "building up docker swarm in aws..."
 aws cloudformation create-stack   \
-      --template-body file://./aws-swarm.json \
+      --capabilities CAPABILITY_IAM \
+      --template-url https://s3-us-west-2.amazonaws.com/updoc-iac/aws-swarm.json\
       --stack-name updoc \
       --parameters ParameterKey=ClusterSize,ParameterValue=1 \
         ParameterKey=EnableCloudStorEfs,ParameterValue=yes \
         ParameterKey=EnableCloudWatchLogs,ParameterValue=yes \
         ParameterKey=EnableEbsOptimized,ParameterValue=no \
         ParameterKey=EnableSystemPrune,ParameterValue=no \
-        ParameterKey=EncryptEFS,ParameterValue=no \
+        ParameterKey=EncryptEFS,ParameterValue=false \
         ParameterKey=InstanceType,ParameterValue=t2.micro \
         ParameterKey=KeyName,ParameterValue=updoc \
         ParameterKey=ManagerDiskSize,ParameterValue=20 \
@@ -22,7 +26,7 @@ aws cloudformation create-stack   \
 while [[ `aws cloudformation describe-stacks --stack-name updoc --query Stacks[0].StackStatus` != *"COMPLETE"* ]]
 do
     status = $(aws cloudformation describe-stacks --stack-name updoc --query Stacks[0].StackStatus)
-    echo `status is $status``
+    echo `status is $status`
 	sleep 5
 done
 
